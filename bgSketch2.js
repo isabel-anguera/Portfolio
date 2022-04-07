6dc47c9972551252cf3ec8278153eeefa6c81eee
Isabel Anguera <isabelanguera2@gmail.com>
2022-04-07 18:20:12 -0400

    sketch changes

--- bgSketch2.js
+++ bgSketch2.js
@@ -4,17 +4,16 @@ function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
   cnv.position(0, 0);
   cnv.style("z-index", "-1");
-  cnv.style("opacity", ".2");
+  cnv.style("opacity", ".1");
   noStroke();
-  speed = 2;
-  fill(150, random(10, 90));
+  colorMode(HSL);
 }
 
 function draw() {
   background(255);
-  let t = frameCount / 240;
+  let t = frameCount / 60;
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 1; i++) {
     fallingBars.push(new fallingBar());
   }
 
@@ -25,14 +24,14 @@ function draw() {
 }
 
 function fallingBar() {
-  this.posX = random(0, width);
-  this.posY = -50;
-  this.h = random(5, 45);
+  this.posY = random(0, width);
+  this.posX = -50;
+  this.h = random(5, 25);
   this.w = random(5, 10);
+  this.fillColor = color(200, 50, random(50, 80), random(1));
 
   this.update = function (time) {
-    // this.posY += speed;
-    this.posY += pow(this.h, 0.25);
+    this.posX += pow(this.h, 0.25) / 2;
 
     if (this.posY > height + 50) {
       let index = fallingBars.indexOf(this);
@@ -41,6 +40,7 @@ function fallingBar() {
   };
 
   this.display = function () {
-    rect(this.posX, this.posY, this.w, this.h);
+    fill(this.fillColor);
+    ellipse(this.posX, this.posY, this.h, this.h);
   };
 }
